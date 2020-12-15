6be9bca1f3b1de44c4eef2ca4ea335c0b536eb6b
treguv <vladislavtregubov00@gmail.com>
2020-12-15 00:23:42 -0800

    Added card tracking page

--- public/assets/js/tracking.js
+++ public/assets/js/tracking.js
@@ -1,14 +1,18 @@
 //make function that dynamically generates a bootstrap card and adds to page
 function generateCard(trackingId) {
   let trackingCardCol = $("<div>"); // make column
-  trackingCardCol.addClass("col-sm-1 col-lg-4 my-2");
+  trackingCardCol.addClass("col-sm-1 col-md-4 my-2");
   //make card to add to the col
-  let trackingCard = $("<div>").addClass("card text-center");
+  let trackingCard = $("<div>").addClass("card text-center card-background");
   //make card body
-  let trackingCardBody = $("<div>").addClass("card-body");
+  let trackingCardBody = $("<div>").addClass("card-body card-content");
   //add title and text to body
-  let trackingCardHeader = $("<div>").addClass("card-titlte").text(trackingId);
-  let trackingCardBodyText = $("<div>").addClass("card-text").text("Track Me!");
+  let trackingCardHeader = $("<div>")
+    .addClass("card-title header-text inverted-color")
+    .text(trackingId);
+  let trackingCardBodyText = $("<div>")
+    .addClass("card-text header-sub-text inverted-color ")
+    .text("Track Me!");
   //append to card body
   trackingCardBody.append(trackingCardHeader);
   trackingCardBody.append(trackingCardBodyText);
@@ -19,6 +23,6 @@ function generateCard(trackingId) {
   $("#tracking-card-row").append(trackingCardCol);
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 25; i++) {
   generateCard(i);
 }
