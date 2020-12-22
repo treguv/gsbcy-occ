3a989112b88924aca6bcabce3493e5dfbfd3226b
treguv <vladislavtregubov00@gmail.com>
2020-12-22 00:32:32 -0800

    added page based display system for tracking

--- public/assets/js/tracking.js
+++ public/assets/js/tracking.js
@@ -1,7 +1,26 @@
+//house all the data
+let cardIds = [];
+//set up a tracker for the page number
+let pageNumber = 0;
+
+//generate 10 cards based on the page number
+function generateByPageNumbers() {
+  //clear the section
+  $("#tracking-card-row").empty();
+  let startingNum = pageNumber * 10;
+  //Calculate the 10 cards to be made
+  for (let i = startingNum; i < startingNum + 9; i++) {
+    if (i <= cardIds.length) {
+      generateCard(cardIds[i].code);
+    }
+    //console.log(cardIds[i].code);
+  }
+}
 //make function that dynamically generates a bootstrap card and adds to page
 function generateCard(trackingId) {
   let trackingCardCol = $("<div>"); // make column
   trackingCardCol.addClass("col-sm-1 col-md-4 my-2");
+  trackingCardCol.attr("id", trackingId);
   //make card to add to the col
   let trackingCard = $("<div>").addClass("card text-center card-background");
   //make card body
@@ -37,9 +56,9 @@ fetch("/tracking", {
     //console.log(resParsed[10]);
     // console.log(JSON.parse(resParsed));
     //probably a bad way to do it
-    for (let i = 0; i < resParsed.length; i++) {
-      generateCard(resParsed[i].code);
-    }
+    cardIds = resParsed;
+    generateByPageNumbers();
+    //console.log(cardIds);
   });
 
 function cardCreation(data) {
@@ -47,3 +66,17 @@ function cardCreation(data) {
   var newData = data.body;
   console.log(newData);
 }
+// Add listeners for the next and previous buttons
+$("#prev-page-btn").on("click", function (event) {
+  if (pageNumber > 0) {
+    pageNumber--;
+    generateByPageNumbers();
+  }
+});
+$("#next-page-btn").on("click", function (event) {
+  if (pageNumber < 12) {
+    pageNumber++;
+    generateByPageNumbers();
+    console.log(pageNumber);
+  }
+});
